6c819e6c6a0cb1a65914743e2df0d581303b3f9c
Guillaume ROTH <guillaume.roth7@etu.univ-lorraine.fr>
2016-11-12 23:57:39 +0100

    Rajout d'un bouton sur l'accueil

--- application/views/game.php
+++ application/views/game.php
@@ -8,5 +8,8 @@
             <form method = 'POST' action='http://localhost/LoveLetter/index.php/gamecontroller/cartes'>
                 <input type ='submit' value="Voir les cartes"/>
             </form>
+            <form method = 'POST' action='http://localhost/LoveLetter/index.php/gamecontroller/cartesTraduites'>
+                <input type ='submit' value="Voir les cartes et leur traduction"/>
+            </form>
         </body>
 </html>
