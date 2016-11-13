cad8f4602a877f0dcf8cb944d3926f0ed671fff3
Guillaume ROTH <guillaume.roth7@etu.univ-lorraine.fr>
2016-11-13 21:02:50 +0100

    Qqes trucs

--- application/controllers/gamecontroller.php
+++ application/controllers/gamecontroller.php
@@ -4,7 +4,6 @@ class GameController extends CI_Controller{
 	function __construct(){
 		parent::__construct();
 		$this->load->model('gamemodel');
-                $this->load->library('image_lib');
 
 	}
 	
--- application/views/game.php
+++ application/views/game.php
@@ -5,6 +5,20 @@
 	</head>
 	
 	<body>
-            <h1> Bienvenue sur LoveLetter ! </h1>
-        <center><a href="http://localhost/LoveLetter/index.php/gamecontroller/cartesImages"><img src="http://localhost/LoveLetter/upload/backCard.png" height="250" width="175" class="img-responsible"/></a></center>
+            <h1> LoveLetter</h1>
+            <center><a href="http://localhost/LoveLetter/index.php/gamecontroller/cartesImages"><img src="http://localhost/LoveLetter/upload/backCard.png" height="250" width="175" class="img-responsible"/></a></center>
+        
+            <h2>How to Play</h2>
+
+            <p>There are eight different types of cards, shown below. Each card has a number, and an effect when played.</p>
+
+            <p>Each player holds one card in their hand. When it's your turn, you draw a card, then play a card. </p>
+
+            <p>Some cards end the round immediately and determine who wins. Others simply have an effect on gameplay. </p>
+
+            <p>If no one ends the round by playing a card which ends the round, then the player holding the highest-value card at the end (when the deck runs out) wins. </p>
+
+            <p>The winner of each round earns a point. The first player to earn seven points wins the game. Happy playing!</p>
+        
+        </body>
 </html>
